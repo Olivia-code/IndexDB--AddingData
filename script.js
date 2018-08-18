45a2fbc083a6a39af436468bc7b809c9068de5da
Olivia <olivia.devops@gmail.com>
2018-08-18 11:59:11 -0700

    Adding updated data operation. To update data in object store by using put() function.

--- script.js
+++ script.js
@@ -72,4 +72,87 @@ window.onload = function(){
       }
     });
 
+    $('#fetch-book-button').click((e)=>{
+       var isbn = $('#isbn-input-box').val();
+       if(isbn) {
+        fetchBook(isbn);
+       }else {
+           alert("please enter ISBN");
+       }
+    });
+
+    $("#update-book-button").click((e)=> {
+        event.preventDefault();
+        var isbn = $("#isbn").val();
+        var name = $("#name").val();
+        var publication = $("#year").val();
+
+        var book = {
+            isbn: isbn,
+            name: name,
+            publication: publication, 
+            updatedAt: new Date().getTime()
+        };
+
+        updateBook(book);
+        
+    });
+
+    function fetchBook(isbn) {
+        // Handling DataBase
+
+        var db = dbRequest.result;
+
+        // Creating Transaction
+
+        var transaction = db.transaction("books", "readonly");
+
+        // Handling Object Store
+
+        var store = transaction.objectStore("books");
+
+        // Retrive Object
+
+        var request = store.get(isbn);
+
+        request.onsuccess = ((e)=> {
+            var book = e.target.result;
+
+            if(book) {
+                fillUpdateForm(book)
+            }else{
+                alert("There is no book with this ISBN")
+            }
+        });
+    }
+
+    function fillUpdateForm(book) {
+        $("#isbn").val(book.isbn);
+        $("#name").val(book.name);
+        $("#year").val(book.publication);
+        
+    };
+    function updateBook(book){
+
+        // Handling DataBase
+
+        var db = dbRequest.result;
+
+        // Creating Transaction
+
+        var transaction = db.transaction("books", "readwrite");
+
+        // Handling Object Store
+
+        var store = transaction.objectStore("books");
+
+        // Update Object
+        var request = store.put(book); // put() is using for replacing the old object with the new one
+
+        request.onsuccess = (() => {
+            alert("Object is updated")
+        }); 
+        
+    }
+
 }
